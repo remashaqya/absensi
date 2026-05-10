96247a6108d89b6eefcb06e71f273cd25ef1a67f
remashaqya <remashaqqulyaqindinoyo.sby@gmail.com>
2026-05-10 23:18:49 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+// ==========================================
+// FILE: data.js
+// Kumpulan data ID Anggota dan Keterangan Opsional
+// Format: "ID-BARCODE": { nama: "Nama Lengkap", keterangan: "Divisi/Jabatan" }
+// ==========================================
+
+const dataAnggota = {
+    "RW-001": { nama: "Rudi Yanto", keterangan: "Perlengkapan" },
+    "RW-002": { nama: "Siti Aisyah", keterangan: "Konsumsi" },
+    "RW-003": { nama: "Dian Sastrowardoyo" }, // Keterangan dikosongkan tidak masalah
+    "RW-004": { nama: "Reza Rahadian", keterangan: "Keamanan" }
+    // Tambahkan baris baru untuk anggota lainnya di sini...
+};
