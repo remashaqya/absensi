aa8f21ce6d858bdd558f6942a171f36959419e46
remashaqya <remashaqqulyaqindinoyo.sby@gmail.com>
2026-05-12 18:35:29 +0700

    Update data.js

--- data.js
+++ data.js
@@ -5,9 +5,27 @@
 // ==========================================
 
 const dataAnggota = {
-    "RW-001": { nama: "Rudi Yanto", keterangan: "Perlengkapan" },
-    "RW-002": { nama: "Siti Aisyah", keterangan: "Konsumsi" },
-    "RW-003": { nama: "Dian Sastrowardoyo" }, // Keterangan dikosongkan tidak masalah
-    "RW-004": { nama: "Reza Rahadian", keterangan: "Keamanan" }
-    // Tambahkan baris baru untuk anggota lainnya di sini...
+    "PQHY001": { nama: "Agung Sudaryanto", keterangan: "Pembina" },
+    "PQHY002": { nama: "Ahmad Rendi N.", keterangan: "Ketua Umum" },
+    "PQHY003": { nama: "Nur Rokhim", keterangan: "Ketua Pelaksana" },
+    "PQHY004": { nama: "Siswanto", keterangan: "Wakil Ketupel" },
+    "PQHY005": { nama: "Rizkyah Amanatul R.", keterangan: "Sekretaris" },
+    "PQHY006": { nama: "Atfiyatul Fatati", keterangan: "Bendahara" },
+    "PQHY007": { nama: "Ani Nuraini", keterangan: "Humas & Dana" },
+    "PQHY008": { nama: "Meishah Gelis", keterangan: "Humas & Dana" },
+    "PQHY009": { nama: "Muhda Sabila", keterangan: "Humas & Dana" },
+    "PQHY010": { nama: "Rini Hardiani", keterangan: "Humas & Dana" },
+    "PQHY011": { nama: "Annisa Herawati", keterangan: "Humas & Dana" },
+    "PQHY012": { nama: "Ninis Fadillah", keterangan: "Kord. Pawai" },
+    "PQHY013": { nama: "Fajar Subkhi Fatwan", keterangan: "Transport & Keamanan" },
+    "PQHY014": { nama: "Dimas Tirta A.", keterangan: "Transport & Keamanan" },
+    "PQHY015": { nama: "Choirul Hidayat", keterangan: "Transport & Keamanan" },
+    "PQHY016": { nama: "Moch. Sidik Akbar", keterangan: "Transport & Keamanan" },
+    "PQHY017": { nama: "Sena Fahriantono", keterangan: "Publikasi & Dokumentasi" },
+    "PQHY018": { nama: "Bilqis Oktavia", keterangan: "Publikasi & Dokumentasi" },
+    "PQHY019": { nama: "Alya Meyfa Putri", keterangan: "Publikasi & Dokumentasi" },
+    "PQHY020": { nama: "Alif Asfa Julian A.", keterangan: "Perlengkapan" },
+    "PQHY021": { nama: "Ahmad Alam Mahesa", keterangan: "Perlengkapan" },
+    "PQHY022": { nama: "Rofi'ut Tilawatil M.", keterangan: "Konsumsi & Kesehatan" },
+    "PQHY023": { nama: "Aida Rosidatul M.", keterangan: "Konsumsi & Kesehatan" }
 };
